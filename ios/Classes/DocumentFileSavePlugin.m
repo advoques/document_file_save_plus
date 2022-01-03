60c5cd5e6e0641e4581fa1483aa67910533b312d
nifal.nizar <nifal.nizar@htworkslk.com>
2022-01-03 14:15:00 +0530

    ios fixes

--- ios/Classes/DocumentFileSavePlugin.m
+++ ios/Classes/DocumentFileSavePlugin.m
@@ -1,11 +1,11 @@
 #import "DocumentFileSavePlugin.h"
-#if __has_include(<document_file_save/document_file_save-Swift.h>)
-#import <document_file_save/document_file_save-Swift.h>
+#if __has_include(<document_file_save_plus/document_file_save_plus-Swift.h>)
+#import <document_file_save_plus/document_file_save_plus-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "document_file_save-Swift.h"
+#import "document_file_save_plus-Swift.h"
 #endif
 
 @implementation DocumentFileSavePlugin
